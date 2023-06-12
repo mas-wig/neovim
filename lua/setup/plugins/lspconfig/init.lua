8f63032b20519d60c540ed2e07d80ffeeba8ee23
mas-wig <mas_wig@protonmail.ch>
2023-06-12 18:59:45 +0700

    add border lsp ui stuff

--- lua/setup/plugins/lspconfig/init.lua
+++ lua/setup/plugins/lspconfig/init.lua
@@ -20,8 +20,9 @@ M.ui = function()
 	local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 	function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
 		opts = opts or {}
-		opts.border = opts.border or "single"
+		opts.border = opts.border or "rounded"
 		opts.max_width = opts.max_width or 90
+		opts.wrap = true
 		return orig_util_open_floating_preview(contents, syntax, opts, ...)
 	end
 end
