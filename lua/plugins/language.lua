82e504c0de169471fe16569eee479353c658c0c8
mas-wig <mas_wig@protonmail.ch>
2023-06-13 23:27:48 +0700

    remove ui stuff

--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -4,12 +4,6 @@ return {
 		branch = "master",
 		ft = { "go", "gomod" },
 		build = ":GoInstallBinaries",
-		dependencies = {
-			"ray-x/guihua.lua",
-			branch = "master",
-			build = "cd lua/fzy && make",
-			lazy = true,
-		},
 		config = function()
 			require("setup.plugins.go").init()
 			require("setup.plugins.go").setup()
